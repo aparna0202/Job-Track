ef9a129980a534689ea80f05f400d8e9510d2920
aparna0202 <aparna.pandey172004@gmail.com>
2022-12-25 17:06:57 +0530

    Added the theme file

---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    fontFamily: {
+      sanspro: ["Source Sans Pro", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
